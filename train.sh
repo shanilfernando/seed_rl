d58bf139a730ba453a24c4937ab35978ee298222
shanil.fernando <shanil.fernando@thoughtworks.com>
2020-10-07 19:44:43 +0800

    change python version

--- train.sh
+++ train.sh
@@ -9,8 +9,8 @@ shift 3
 # Start actor tasks which run environment loop.
 actor=0
 while [ "$actor" -lt ${NUM_ACTORS} ]; do
-  python ${ENVIRONMENT}/${AGENT}_main.py --run_mode=actor --logtostderr --total_environment_frames=10000 --game=11_vs_11_kaggle --reward_experiment=scoring,checkpoints --logdir=${PATH}/kaggle_simulations/agent/ --num_actors=${NUM_ACTORS} --task=${actor} 2>/dev/null >/dev/null &
+  python3 ${ENVIRONMENT}/${AGENT}_main.py --run_mode=actor --logtostderr --total_environment_frames=10000 --game=11_vs_11_kaggle --reward_experiment=scoring,checkpoints --logdir=${PATH}/kaggle_simulations/agent/ --num_actors=${NUM_ACTORS} --task=${actor} 2>/dev/null >/dev/null &
   actor=$(( actor + 1 ))
 done
 # Start learner task which performs training of the agent.
-python ${ENVIRONMENT}/${AGENT}_main.py --run_mode=learner --logtostderr --total_environment_frames=10000 --game=11_vs_11_kaggle --reward_experiment=scoring,checkpoints --logdir=${PATH}/kaggle_simulations/agent/ --num_actors="${NUM_ACTORS}"
+python3 ${ENVIRONMENT}/${AGENT}_main.py --run_mode=learner --logtostderr --total_environment_frames=10000 --game=11_vs_11_kaggle --reward_experiment=scoring,checkpoints --logdir=${PATH}/kaggle_simulations/agent/ --num_actors="${NUM_ACTORS}"
